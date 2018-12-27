0266181427de9e485eb7b3aa29e26c4cddbb0e39
Jack <lijaack@yahoo.com>
2018-12-27 00:01:55 -0800

    config set up

--- server.js
+++ server.js
@@ -5,3 +5,16 @@ var bodyParser = require("body-parser");
 var PORT = process.env.PORT || 8080;
 
 var app = express();
+
+
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+
+// require("./routing/apiRoutes")(app);
+// require("./routing/htmlRoutes")(app);
+
+
+// app.listen(PORT, function() {
+//     console.log("App listening on PORT: " + PORT);
+// });
