970f5a37787f7abaa31d963a0e439e4538b6a59f
Jack <lijaack@yahoo.com>
2018-12-26 23:46:57 -0800

    install packages and created DB

---
+++ server.js
@@ -0,0 +1,7 @@
+var express = require("express");
+var exphbs = require("express-handlebars");
+var bodyParser = require("body-parser");
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
