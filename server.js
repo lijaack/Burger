c5713e7ad44a58e4176d5c4e886a569404f811c8
Jack <lijaack@yahoo.com>
2018-12-28 22:59:51 -0800

    added index handlebars

--- server.js
+++ server.js
@@ -6,15 +6,17 @@ var PORT = process.env.PORT || 8080;
 
 var app = express();
 
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
+var routes = require("./controllers/burgers_controller.js");
 
-// require("./routing/apiRoutes")(app);
-// require("./routing/htmlRoutes")(app);
+app.use(routes);
 
-
-// app.listen(PORT, function() {
-//     console.log("App listening on PORT: " + PORT);
-// });
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
